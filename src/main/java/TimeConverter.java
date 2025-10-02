0c592b3fed072bf1a1b47cf6b0f4ea221a62851e
Michael-Meerovich <03michael.meerovich@kamsc.org>
2025-10-02 13:59:45 -0400

    Update TimeConverter.java

--- src/main/java/TimeConverter.java
+++ src/main/java/TimeConverter.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.String;
 
 public class TimeConverter {
     public static void main(String[] args) {
@@ -8,17 +9,38 @@ public class TimeConverter {
         // - Display conversion results and calculation breakdown
         // - Ask user if they want to convert another time (y/n)
         // - Loop until user chooses to exit
-        double hours, minutes, seconds;
+        int hours, minutes, seconds, hourseconds, minuteseconds, total;
         boolean run = true;
         Scanner sc = new Scanner(System.in);
-        while (run)
-        {
-            System.out.println ("Convert time to total seconds\n");
-            System.out.print ("Enter hours:  ");
-            hours = sc.nextDouble ();
-            System.out.print ("\nEnter minutes:  ");
-            minutes = sc.nextDouble ();
-            System.out.print ("\nEnter seconds:  ");
+        while (run) {
+            System.out.println("Convert time to total seconds\n");
+            System.out.print("Enter hours:  ");
+            hours = sc.nextInt();
+            System.out.print("Enter minutes:  ");
+            minutes = sc.nextInt();
+            System.out.print("Enter seconds:  ");
+            seconds = sc.nextInt();
+
+            hourseconds = hours * 3600;
+            minuteseconds = minutes * 60;
+            total = hourseconds + minuteseconds + seconds;
+
+            System.out.println("\nTime Conversion:  ");
+            System.out.println(hours + " hours * 3600 = " + hourseconds + " seconds");
+            System.out.println(minutes + " minutes * 60 = " + minuteseconds + " seconds");
+            System.out.println(seconds + "seconds = " + seconds + " seconds");
+            System.out.println("Total: " + total + " seconds\n");
+
+            System.out.print("Do you want to convert another time?  (y/n):  ");
+            char repeat = sc.next().charAt(0);
+            if (repeat == 'y')
+            {
+                run = true;
+            }
+            else
+            {
+                run = false;
+            }
         }
     }
 }
